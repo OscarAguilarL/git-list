e02b624aa3c4472d90dace318ab6682b708ad57c
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-26 20:36:19 -0600

    Inicio del proyecto y primer componente

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+export const Avatar = () => {
+  return (
+    <img
+      src="https://avatars.githubusercontent.com/u/66934770?v=4"
+      alt=""
+      width="200"
+    />
+  )
+}
---
+++ src/components/User.jsx
@@ -0,0 +1,8 @@
+export const User = ({ children, name }) => {
+  return (
+    <div>
+      {children}
+      <p>Hola mi nombre es {name}</p>
+    </div>
+  )
+}
